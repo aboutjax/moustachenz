ff2915361c5c265c21eb46765117698085c66126
Jacky Lee <aboutjax1990@gmail.com>
2016-04-09 21:44:18 +1200

    Updtae

--- src/script/app.js
+++ src/script/app.js
@@ -5,7 +5,7 @@ $.ajax({
   url: "https://api.instagram.com/v1/tags/moustachenz/media/recent?access_token=401851.1fb234f.918590524c4846daa56ee4e3d654c4e1",
   success: function(data) {
     for (var i = 0; i < 120; i++) {
-      $(".popular").append("<a class='post' target='_blank' href='" + data.data[i].link + "'><img class='post' src='" + data.data[i].images.low_resolution.url +"'></img></a>");
+      $(".mo-popular").append("<a class='mo-post' target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
     }
   }
 });
