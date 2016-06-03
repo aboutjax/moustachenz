cdea93e12a048db3cfe07f7b2f5babb11a5e5989
Jacky Lee <aboutjax1990@gmail.com>
2016-06-03 23:19:17 +1200

    Update and remove insta

--- src/script/app.js
+++ src/script/app.js
@@ -1,20 +1,20 @@
-$.ajax({
-  type: "GET",
-  dataType: "jsonp",
-	cache: false,
-  url: "https://api.instagram.com/v1/tags/moustachenz/media/recent?access_token=401851.1fb234f.918590524c4846daa56ee4e3d654c4e1",
-  success: function(data) {
-    for (var i = 0; i < 120; i++) {
-      $(".mo-popular").append("<a class='mo-post' target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
-    }
-  }
-});
+// $.ajax({
+//   type: "GET",
+//   dataType: "jsonp",
+// 	cache: false,
+//   url: "https://api.instagram.com/v1/tags/moustachenz/media/recent?access_token=401851.fd162e7.00fcbf8111894411aa34a78a0a41729f",
+//   success: function(data) {
+//     for (var i = 0; i < 120; i++) {
+//       $(".mo-popular").append("<a class='mo-post' target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
+//     }
+//   }
+// });
 
 $.ajax({
   type: "GET",
   dataType: "json",
 	cache: false,
-  url: "https://sheetsu.com/apis/v1.0/6ff573d5",
+  url: "/src/script/hours.js",
   success: function(times) {
     for (var i = 0; i < times.length; i++) {
       var time = times[i]
---
+++ src/script/hours.js
@@ -0,0 +1,3 @@
+[
+  {"location":"Auckland University Quad","days":"Mon - Fri","hours":"10am - 8pm","days2":"Sat","hours2":"12pm - 6pm"}
+]
